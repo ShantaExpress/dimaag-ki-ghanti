0e9843b208cb6b30250673304e3d118b51fe1b4c
Avay Minni <avayminni@hotmail.com>
2019-01-24 01:40:26 +0530

    Use index html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  // res.sendFile('index.html');
+  res.sendFile(path.resolve(__dirname , '../views/index.html'));
+  // res.sendFile(path.resolve(__dirname + '/index.html', '../'));
+  // res.send(__dirname);
 });
 
 module.exports = router;
